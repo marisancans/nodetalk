29a48b38aa98869247934b51fcb4286ea571e8ec
ma <marisancans@gmail.com>
2018-09-23 22:11:22 +0300

    MQTT first test

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,62 @@
 // #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
+#include <PubSubClient.h>
 
 const char* ssid = "kasteste";
 const char* password = "punkaripa";
+const char* mqttServer = "178.128.197.152";
 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
+ 
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
   }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
+  
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP32Client")) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.subscribe("esp/test");
 
   // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
@@ -68,7 +109,7 @@ void setup() {
 }
 
 void loop() {
-  ArduinoOTA.handle();
-  Serial.println("OTA test");
-  delay(1000);
+    ArduinoOTA.handle();
+    client.loop();
+    delay(1000);
 }
